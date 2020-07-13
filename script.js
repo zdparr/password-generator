e54baf53283b3f7b0e28d3c3c117aef3bb814fe6
Zach Parr <zachery.parr@gmail.com>
2020-07-13 16:44:51 -0500

    creating password scaffold

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function to generate password
+function generatePassword() {
+  let length = prompt("How many characters do you want?");
+  let lowerCase = confirm("Do you want lower case characters?");
+  let upperCase = confirm("Do you want upper case characters?");
+  let specialCharacters = confirm("Do you want special characters?");
+  console.log(length);
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
