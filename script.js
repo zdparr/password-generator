2b8b6b9371c0383630478c583fd96142b6306af8
Zach Parr <zachery.parr@gmail.com>
2020-07-14 23:02:25 -0500

    updating javascript generate fuction

--- script.js
+++ script.js
@@ -3,11 +3,41 @@ var generateBtn = document.querySelector("#generate");
 
 // Function to generate password
 function generatePassword() {
-  let length = prompt("How many characters do you want?");
-  let lowerCase = confirm("Do you want lower case characters?");
-  let upperCase = confirm("Do you want upper case characters?");
-  let specialCharacters = confirm("Do you want special characters?");
-  console.log(length);
+  // Required variables
+  const passwordLength  = prompt("How many characters do you want?");
+  const upperCaseReq    = confirm("Do you want to use uppercase?");
+  const numberReq       = confirm("Do you want to use numbers?");
+  const specialCharReq  = confirm("Do you want to use special characters?");
+  let returnedPassword  = "";
+  
+
+  // Arrays of data
+  const lowerCaseChars  = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseChars  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars     = "0123456789";
+  const specialChars    = "!@#$%^&*";
+
+  // Build correct array based on desired requirements
+  let charSet = lowerCaseChars;
+  if (upperCaseReq) {
+  charSet = charSet.concat(upperCaseChars);
+  } 
+  if (numberReq) {
+    charSet = charSet.concat(numberChars);
+  }
+  if (specialCharReq) {
+    charSet = charSet.concat(specialChars);
+  }
+  console.log(charSet);
+
+  // Generate password
+  for (let i = 0; i < passwordLength; i++) {
+    
+    charSet = charSet.toString();
+    character = charSet.charAt(Math.floor(Math.random() * charSet.length));
+    returnedPassword = returnedPassword.concat(character);  
+  }
+  return returnedPassword;
 }
 
 // Write password to the #password input
